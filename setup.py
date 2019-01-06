e0a7af95070651e10181d21b019b05d1b2a473a3
manthanchauhan <manthanchauhan913@gmail.com>
2019-01-06 17:35:40 +0530

    second

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='package_making_practice',
+      version='1.0',
+      description='just a copy of pyler',
+      author='manthan chauhan',
+      author_email='manthanchauhan913@gmail.com',
+      packages=['package_making_practice'],
+      zip_safe=False,
+      url='https://github.com/manthanchauhan/package_making_practice'
+      )
